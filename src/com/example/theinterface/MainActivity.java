f4adca24a316eb2d13cbcbc7ea278ca7cb4a8dd9
Niklas Johansson <Niklas_Johansson92@hotmail.com>
2014-04-15 00:05:56 +0200

    Updated to support the Polar coordinate system + added a simple circle as background

--- src/com/example/theinterface/MainActivity.java
+++ src/com/example/theinterface/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.theinterface;
 
+import java.text.DecimalFormat;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -16,7 +18,8 @@ import android.os.Build;
 import android.provider.Contacts.Intents;
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	//bara för att testa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,30 +36,43 @@ public class MainActivity extends ActionBarActivity {
     public void changeIt (View view, float x, float y){
     	TextView theXView = (TextView)findViewById(R.id.xView);
     	TextView theYView = (TextView)findViewById(R.id.yView);
+    	TextView theRView = (TextView)findViewById(R.id.rView);
+    	TextView theAngView = (TextView)findViewById(R.id.angView);
     	
     	x = (x-(480/2));
-    	y = (-y+(800/2));
+    	y = (-y+(800/2)+55);
     	double dx = 0.1*x;
     	double dy = 0.1*y;
     	
+    	double r = 0;
+    	if (!(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))>10)){
+    		r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    	}else{
+    		r= 10;
+    	}
     	
-    	if (dx>=10){
-    		dx=10;
-    	}
-    	else if (dx<=-10){
-    		dx = -10;
-    	}
-    	if (dy>=10){
-    		dy=10;
-    	}
-    	else if (dy<=-10){
-    		dy = -10;
-    	}
-    		
-    	String xS = String.valueOf(dx);
-    	String yS = String.valueOf(dy);
+    	double ang = 57.3*Math.atan2(dy,dx);
+    	
+    //Print out with just two decimals
+    	//change to two decimals
+    	DecimalFormat newFormat = new DecimalFormat("#.##");
+    	
+    	double rWTD =  Double.valueOf(newFormat.format(r));
+    	double angWTD =  Double.valueOf(newFormat.format(ang));
+    	double xWTD =  Double.valueOf(newFormat.format(dx));
+    	double yWTD =  Double.valueOf(newFormat.format(dy));
+    	
+    	//Turn the doubles into strings
+    	String xS = String.valueOf(xWTD);
+    	String yS = String.valueOf(yWTD);
+    	String rS = String.valueOf(rWTD);
+    	String angS = String.valueOf(angWTD);
+    	
+    	//print out the strings into the corresponding views
     	theXView.setText(xS);
     	theYView.setText(yS);
+    	theRView.setText(rS);
+    	theAngView.setText(angS);
     }
     
     public boolean onTouchEvent(MotionEvent event){
