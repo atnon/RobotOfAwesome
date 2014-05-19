449c787e692f022ee91726881ae9e43a4f23e25e
Anton Landberg <git@atnon.se>
2014-05-19 03:29:51 +0200

    Reworked pointing part.
    Reworked in order to only trigger onTouch events for the actual control circle.
    The radius values are now on the range [0,1].
    Should be pretty generic concerning image size and screen size,
    since the values are now normalized.
    
    Powered by Drum and Bass.
    
    Signed-off-by: Anton Landberg <git@atnon.se>

--- src/com/example/theinterface/MainActivity.java
+++ src/com/example/theinterface/MainActivity.java
@@ -3,23 +3,20 @@ package com.example.theinterface;
 import java.text.DecimalFormat;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.os.Build;
-import android.provider.Contacts.Intents;
 
 public class MainActivity extends ActionBarActivity {
 	
-	//bara för att testa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,64 +29,6 @@ public class MainActivity extends ActionBarActivity {
         }
     }
     
-
-    public void changeIt (View view, float x, float y){
-    	TextView theXView = (TextView)findViewById(R.id.xView);
-    	TextView theYView = (TextView)findViewById(R.id.yView);
-    	TextView theRView = (TextView)findViewById(R.id.rView);
-    	TextView theAngView = (TextView)findViewById(R.id.angView);
-    	
-    	x = (x-(480/2));
-    	y = (-y+(800/2)+55);
-    	double dx = 0.1*x;
-    	double dy = 0.1*y;
-    	
-    	double r = 0;
-    	if (!(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))>10)){
-    		r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-    	}else{
-    		r= 10;
-    	}
-    	
-    	double ang = 57.3*Math.atan2(dy,dx);
-    	
-    //Print out with just two decimals
-    	//change to two decimals
-    	DecimalFormat newFormat = new DecimalFormat("#.##");
-    	
-    	double rWTD =  Double.valueOf(newFormat.format(r));
-    	double angWTD =  Double.valueOf(newFormat.format(ang));
-    	double xWTD =  Double.valueOf(newFormat.format(dx));
-    	double yWTD =  Double.valueOf(newFormat.format(dy));
-    	
-    	//Turn the doubles into strings
-    	String xS = String.valueOf(xWTD);
-    	String yS = String.valueOf(yWTD);
-    	String rS = String.valueOf(rWTD);
-    	String angS = String.valueOf(angWTD);
-    	
-    	//print out the strings into the corresponding views
-    	theXView.setText(xS);
-    	theYView.setText(yS);
-    	theRView.setText(rS);
-    	theAngView.setText(angS);
-    }
-    
-    public boolean onTouchEvent(MotionEvent event){
-    	
-    	float x = event.getX();
-    	float y = event.getY();
-    	changeIt(getCurrentFocus(),x,y);
-    	
-    	
-    	return true;
-    }
-    	
-    	
-    
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
@@ -110,18 +49,75 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
+        
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            
+            /* Find the ImageView containing the circle. */
+        	ImageView circleView = (ImageView) rootView.findViewById(R.id.imageView1);
+
+        	/* Create an OnToch listener with its associated onTouch method. */
+        	circleView.setOnTouchListener(new OnTouchListener() {
+        		
+        		@Override
+        		public boolean onTouch(View v, MotionEvent event) {
+        			
+        			/* Set default values. */
+        			double x = 0; 
+        			double y = 0; 
+        			double radius = 0;
+        			double angle = 0;
+        			
+        			if(event.getAction() == android.view.MotionEvent.ACTION_UP) {
+        				/* The user is no longer touching the action area. 
+        				 * Reset the values to 0. */
+        				
+        			} else {
+        				/* User is touching image. */
+        				ImageView circleView = (ImageView) getView().findViewById(R.id.imageView1);
+            			
+            			/* Get touch coordinates. 
+            			 * The coordinates are relative to the top left corner of the image. */
+            			double touchX = event.getX();
+            			double touchY = event.getY();
+            			
+            			/* Calculate image height and width. */
+            			double imageWidth = circleView.getWidth();
+            			double imageHeight = circleView.getHeight();
+            			
+            			/* Calculate the resulting vector and normalize it in order to compensate for 
+            			 * different display sizes. */
+            			x = (touchX - (imageWidth/2))/(circleView.getWidth()/2);
+            			y = -((touchY - (imageHeight/2))/(imageHeight/2));
+            			
+            			/* Calculate vector length, max is 1. */
+            			double vectorLength = Math.sqrt(x*x + y*y);
+            			radius = Math.min(vectorLength, 1);
+            			
+            			/* Calculate vector angle and convert it to degrees. */
+            			angle = (180/Math.PI)*Math.atan2(y, x);
+        			}
+        			
+        			/* Print the values to the screen. */
+        			TextView theRView = (TextView) getView().findViewById(R.id.rView);
+        	    	TextView theAngView = (TextView) getView().findViewById(R.id.angView);
+        	    	TextView theXView = (TextView) getView().findViewById(R.id.xView);
+        	    	TextView theYView = (TextView) getView().findViewById(R.id.yView);
+        	    	
+        	    	DecimalFormat style = new DecimalFormat("#.##");
+        	    	
+        			theRView.setText(style.format(radius));
+        	    	theAngView.setText(style.format(angle));
+        	    	theXView.setText(style.format(x));
+        	    	theYView.setText(style.format(y));
+        			
+        			return true;
+        		}
+        	});
+            
             return rootView;
         }
     }
