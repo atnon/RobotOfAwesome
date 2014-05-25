f77c1403584d6f04578bb89ef4e9cd626249d2df
Anton Landberg <git@atnon.se>
2014-05-25 05:17:30 +0200

    Unit tests for MotorControl.
    Now testing MotorControl, not everything can be tested as of now,
    but something is better than nothing.
    
    Signed-off-by: Anton Landberg <git@atnon.se>

---
+++ src/com/example/theinterface/MotorControlTest.java
@@ -0,0 +1,36 @@
+package com.example.theinterface;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MotorControlTest {
+
+	
+	@Test
+	public void testGenerateControlString() {
+		
+		MotorControl dut = new MotorControl(null);
+		
+		/* Check full forward speed. */
+		assertArrayEquals("set m1speed 127\n".getBytes(), dut.generateControlString(1, 1));
+		assertArrayEquals("set m2speed 127\n".getBytes(), dut.generateControlString(2, 1));
+		
+		/* Check zero speed. */
+		assertArrayEquals("set m1speed 0\n".getBytes(), dut.generateControlString(1, 0));
+		assertArrayEquals("set m2speed 0\n".getBytes(), dut.generateControlString(2, 0));
+		
+		/* Check full reverse speed. */
+		assertArrayEquals("set m1speed -127\n".getBytes(), dut.generateControlString(1, -1));
+		assertArrayEquals("set m2speed -127\n".getBytes(), dut.generateControlString(2, -1));
+	}
+
+	@Test
+	public void testClipRange() {
+		MotorControl dut = new MotorControl(null);
+		assertEquals(dut.clipRange(3.13, -1, 1), 1, 0);
+		assertEquals(dut.clipRange(-3.13, -1, 1), -1, 0);
+		assertEquals(dut.clipRange(0, -1, 1), 0, 0);
+	}
+
+}
